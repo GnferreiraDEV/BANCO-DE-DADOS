cc6516d19c116cb7f31e8dd61d4a4b2f37ffd9fb
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-25 16:09:53 -0300

    UPDATE

--- REVISÃO/SQL/ESCOLA.sql
+++ REVISÃO/SQL/ESCOLA.sql
@@ -44,3 +44,43 @@ CREATE TABLE Avaliacao (
     data_avaliacao DATE,
     FOREIGN KEY (id_matricula) REFERENCES Matricula(id_matricula)
 );
+
+INSERT INTO Professor (nome, email, especialidade) 
+VALUES 
+('Ricardo Almeida', 'ricardo.almeida@example.com', 'Matemática Discreta'),
+('Patrícia Silva', 'patricia.silva@example.com', 'Desenvolvimento Web'),
+('Felipe Gomes', 'felipe.gomes@example.com', 'Engenharia de Software');
+
+INSERT INTO Aluno (nome, email, data_nascimento) 
+VALUES 
+('Juliana Martins', 'juliana.martins@example.com', '1996-08-13'),
+('Fernando Costa', 'fernando.costa@example.com', '1999-01-22'),
+('Isabela Pereira', 'isabela.pereira@example.com', '2001-05-05'),
+('Gabriel Souza', 'gabriel.souza@example.com', '1997-11-30');
+
+INSERT INTO Curso (nome, descricao, id_professor)
+VALUES 
+('Matemática Discreta', 'Curso de fundamentos de Matemática Discreta', 1),
+('Desenvolvimento Web', 'Curso de construção de sites e aplicações web', 2),
+('Engenharia de Software', 'Curso sobre melhores práticas em desenvolvimento de software', 3);
+
+INSERT INTO Matricula (id_aluno, id_curso, data_matricula)
+VALUES 
+(1, 1, '2025-02-22'),
+(2, 1, '2025-02-24'),
+(3, 2, '2025-02-25'),
+(4, 2, '2025-02-26'),
+(1, 3, '2025-02-27'),
+(2, 3, '2025-02-28');
+
+INSERT INTO Avaliacao (id_matricula, nota, data_avaliacao)
+VALUES 
+(1, 9.0, '2025-02-26'),
+(2, 8.0, '2025-02-27'),
+(3, 7.0, '2025-02-28'),
+(4, 8.5, '2025-02-28');
+
+
+
+
+
